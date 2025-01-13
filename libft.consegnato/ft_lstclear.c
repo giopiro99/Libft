4ebb434bc632d3ac3de0c604547847f081ad4f78
Giovanni Pirozzi <gpirozzi@c2r6p8.42firenze.it>
2025-01-13 16:25:35 +0100

    Milestone 0

---
+++ libft.consegnato/ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpirozzi <giovannipirozzi12345@gmail.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 18:03:24 by gpirozzi          #+#    #+#             */
+/*   Updated: 2024/11/27 16:36:36 by gpirozzi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	void	*ptr;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		ptr = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = ptr;
+	}
+}
